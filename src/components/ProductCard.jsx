58732cb8cf730b4a3f6c9af1d601a9bcfd6ae019
Jakeb <tiffanyytg2006@gmail.com>
2026-08-09 18:52:50 -0600

    Se agrego la funcionalidad al boton para que cuando se presione muestre la lista de especificaciones y se le agrego un padding minimo para mejor estetica visual

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -4,6 +4,7 @@
 
   //Se importo la libreria de useState nos de una variable y funcion
   import { useState } from "react"
+  import Button from "./ui/Button"
   function ProductCard({ producto }) {
     //Se declara el estado dentro del componente
     //expandido -> Valor
@@ -19,12 +20,26 @@
         <div className="p-4">
           <h3 className="font-semibold text-lg">{producto.nombre}</h3>
           <p className="text-slate-600 text-sm mt-1">{producto.descripcion}</p>
+          <Button onClick={() => setExpandido(!expandido)}>
+            {expandido ? "Ver menos" : "Ver mas"}
+          </Button>
         </div>
+        {/*En la linea 28 se ocupa la funcion: Object.entries el cual convierte un objeto en un arreglo de pares*/}
+        {expandido && (
+          <div className="p-4">
+            {Object.entries(producto.especificaciones).map(([llave, valor]) => (
+              <p key={llave}>
+                {llave}: {valor}
+              </p>
+            ))}
+          </div>
+        )}
       </div>
+      
     )
   }
 
   export default ProductCard
 
   //se le modifica de producto.imagen a producto.imagenes[0] ya que en el JSON se declaro y uso como array
-
+  // se ocupo operador ternario en la linea 24 para validar la condicion
